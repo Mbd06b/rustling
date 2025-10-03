9c24addf0e89a6c82204bbb66689cbba737f156f
Matthew Dowell <mbd06b+github@gmail.com>
2025-10-03 12:02:55 +0000

    lesson 1 and guessing_game project modules

---
+++ projects/guessing_game/src/main.rs
@@ -0,0 +1,46 @@
+use std::cmp::Ordering;
+use std::io;
+
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!"); 
+    
+    let secret_number = rand::rng().random_range(1..=100); 
+
+    println!("The secret number is: {secret_number}");  
+
+    loop{
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        if guess.trim().to_lowercase() == "quit"  {
+             println!("Goodbye");
+             break;
+        }
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            } 
+        };
+
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break; 
+            }
+        }
+    }
+}
